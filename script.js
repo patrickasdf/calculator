a46f4f28b762b1d159f7eed6857cfea3ff30fef1
patrickasdf <173692702+patrickasdf@users.noreply.github.com>
2025-01-15 19:12:53 +1000

    script.js cleaned up operate function, commented idea for displayPopulate refactor

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ document.body.appendChild(container);
 const display = document.querySelector(".display");
 display.textContent = '';
 currentDisplay = display.textContent;
-console.log("current= " + currentDisplay);
 const clear = document.querySelector(".clear");
 const buttonPress = document.querySelectorAll(".clickme");
 
 // Calculator functions
 function add(a,b) {
     console.log(parseInt(a) + parseInt(b));
-    return (a + b);
+    return a + b;
 }
 
 function subtract(a,b) {
@@ -31,34 +30,44 @@ function divide(a,b) {
 //Object for digits and modifiers (not used yet??)
 const buttonOptions = { 
     digit : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    modifier : ['add', 'subtract', 'multiply', 'divide'],
-    clear: ['clear'],
 }
 
 function displayClear(){ 
     display.textContent = '';
     currentDisplay = '';
     decimalToggle(); //toggle if on, do nothing if off
-    console.log("display reset to nothing" + currentDisplay);
     //firstNUm, operator, secondNum to be cleared also
 }
 clear.addEventListener("click", displayClear);
 
+//numarray
+// array.push(button.textContent)
+//decimals should already be disabled
+//modifiers -- 
+// if !firstVal {
+// array.join(''));
+// array.split(button.textContent) //mod pressed)
+// firstVal = array[0] 
+// first .contains(".") {
+//      parsefloat(firstVal)
+//  }
+// else firstVal = parseInt(firstVal)
+// numarray = [] //reset to 0 after saving firstVal
+//} 
 function displayPopulate() {
+    numArray = [];
+
     buttonPress.forEach((button) => {
         button.addEventListener("click", () => {
             if (!button.classList.contains("disabled")) {
                 if (button.classList.contains("decimal")) {
                     display.textContent = currentDisplay + button.textContent;
                     currentDisplay = display.textContent;
-                   // console.log("Display= " + currentDisplay);
                     button.classList.add("disabled");
                 }
                 else if (button.classList.contains("modifier")) {
                     completedNum = display.textContent;
                     currentDisplay = display.textContent;
-                   // console.log("Display= " + currentDisplay);
-                   // console.log("completedNum = " +completedNum);
                     decimalToggle();
                     display.textContent = '';
                     currentDisplay = display.textContent;
@@ -68,32 +77,29 @@ function displayPopulate() {
                 else if (button.classList.contains("equals")) {
                     completedNumSecond = display.textContent;
                     currentDisplay = display.textContent;
-                    console.log("Display= " + currentDisplay);
                     console.log("completedNum = " +completedNum);
                     console.log("completedNumSecond- " + completedNumSecond);
                     decimalToggle();
-                    display.textContent = operate(completedNum, modifier, completedNumSecond);
-                    currentDisplay = display.textContent;
+                    currentDisplay = operate(completedNum, modifier, completedNumSecond);
+                    display.textContent = currentDisplay;
                 }
                 else {
                     display.textContent = currentDisplay + button.textContent;
                     currentDisplay = display.textContent;
-                    console.log("Display= " + currentDisplay);
                 }
             }
         })
     })
 }
 displayPopulate();
+//each click adds to end of array, check for mod or equals and run operation
 
 function decimalToggle() { //turn off decimal for modifiers, equals, clear
-    if (!document.querySelector(".disabled")) {
-        console.log("no disabled");
+    if (!document.querySelector(".disabled")) { //do nothing
     }
-    else if (document.querySelector(".disabled")) {
+    else { //decimal is disabled so turn back on
         const disabledActive = document.body.querySelector(".disabled");
         disabledActive.classList.remove("disabled");
-        console.log("disabled active");
     }
 }
 
@@ -104,21 +110,9 @@ const operator = 0;
 
 function operate(firstNum, operator, secondNum) {
     switch (operator) {
-        case 0:
-            console.log("case 0, operator 0");
-            break;
-        case '+': //add
-            add(firstNum, secondNum);
-            break;
-        case "-": //subtract
-            subtract(firstNum, secondNum);
-            break;
-        case "*": //multiply
-            multiply(firstNum, secondNum);
-            break;
-        case "/": //divide
-            divide(firstNum, secondNum);
-            break;
+        case "+": return add(firstNum, secondNum);
+        case "-": return subtract(firstNum, secondNum);
+        case "*": return multiply(firstNum, secondNum);
+        case "/": return divide(firstNum, secondNum);
     }
 }
-operate(firstNum, operator, secondNum);
